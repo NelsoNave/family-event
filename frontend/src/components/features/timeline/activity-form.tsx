01842042e02fad0d579201b946c627ffaf57cc6d
Risa Yamamoto <yamari2422@gmail.com>
2025-02-26 01:44:33 -0800

    feat: add logic for creating activity

--- frontend/src/components/features/timeline/activity-form.tsx
+++ frontend/src/components/features/timeline/activity-form.tsx
@@ -10,141 +10,52 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { toast } from "@/hooks/use-toast";
-import { addActivity, updateActivity } from "@/lib/actions/event/timeline";
-import { useAuthAxios } from "@/lib/api/axios-client";
-import { extractTime } from "@/lib/helpers/time-utils";
+import { useToast } from "@/hooks/use-toast";
+import { addActivity } from "@/lib/actions/event/timeline";
 import { showErrorToast } from "@/lib/toast/toast-utils";
-import { EventType } from "@/types/event";
-import { TimelineType } from "@/types/timeline";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { Controller, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 type ActivityFormProps = {
   eventId: string;
-  activityData?: TimelineType;
 };
 
-const FormSchema = z
-  .object({
-    title: z.string().min(2, {
-      message: "Activity title must be at least 2 characters.",
-    }),
-    description: z.string().min(2, {
-      message: "Activity detail must be at least 2 characters.",
-    }),
-    startTime: z.string(),
-    endTime: z.string(),
-  })
-  .refine(
-    (data) => {
-      if (!data.startTime || !data.endTime) return true;
-      return data.startTime < data.endTime;
-    },
-    {
-      message: "End time must be after start time",
-      path: ["endTime"],
-    },
-  );
+const FormSchema = z.object({
+  title: z.string().min(2, {
+    message: "Activity title must be at least 2 characters.",
+  }),
+  description: z.string().min(2, {
+    message: "Activity detail must be at least 2 characters.",
+  }),
+});
 
-export function ActivityForm({ eventId, activityData }: ActivityFormProps) {
-  const router = useRouter();
-  const axios = useAuthAxios();
-  const [eventDate, setEventDate] = useState<EventType["startTime"] | null>(
-    null,
-  );
-
+export function ActivityForm({ eventId }: ActivityFormProps) {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      startTime: activityData ? extractTime(activityData.startTime) : "",
-      endTime: activityData ? extractTime(activityData?.endTime) : "",
-      title: activityData?.title || "",
-      description: activityData?.description || "",
+      title: "",
+      description: "",
     },
   });
 
-  const handleActivitySubmission = async (requestData: {
-    title: string;
-    description: string;
-    startTime: string;
-    endTime: string;
-  }) => {
-    try {
-      if (activityData) {
-        return await updateActivity({
-          requestData,
-          eventId,
-          activityId: activityData?.id,
-        });
-      } else {
-        return await addActivity({ requestData, eventId });
-      }
-    } catch (err) {
-      if (err instanceof Error) {
-        showErrorToast(toast, err, err.message);
-      } else {
-        showErrorToast(
-          toast,
-          err,
-          "An error occurred while processing your request. Please try again.",
-        );
-      }
-    }
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get<{
-          event: EventType;
-        }>(`/events/${eventId}`);
-
-        const eventData = response.data.event;
-
-        if (eventData) {
-          setEventDate(eventData.startTime);
-        }
-      } catch (err) {
-        showErrorToast(
-          toast,
-          err,
-          "Failed to fetch event information. Please try again.",
-        );
-      }
-    };
-
-    fetchData();
-  }, [axios, eventId]);
-
-  if (!eventDate) {
-    return <div>Loading...</div>;
-  }
+  const { toast } = useToast();
+  const router = useRouter();
 
   const onSubmit = async (data: z.infer<typeof FormSchema>) => {
     try {
-      const date = new Date(eventDate);
-      const targetDate = date.toISOString().replace(/T.*Z$/, "");
-      const formattedStartTime = new Date(
-        `${targetDate}T${data.startTime}:00`,
-      ).toISOString();
-      const formattedEndTime = new Date(
-        `${targetDate}T${data.endTime}:00`,
-      ).toISOString();
+      const response = await addActivity({
+        activityData: {
+          title: data.title,
+          description: data.description,
+          startTime: "2025-02-25T19:40:00Z",
+          endTime: "2025-02-25T19:50:00Z",
+        },
+        eventId,
+      });
 
-      const requestData = {
-        title: data.title,
-        description: data.description,
-        startTime: formattedStartTime,
-        endTime: formattedEndTime,
-      };
-
-      const response = await handleActivitySubmission(requestData);
-
-      if (response?.success) {
+      if (response && response.success) {
         router.push(`/event/${eventId}/timeline`);
       }
     } catch (err) {
@@ -155,56 +66,6 @@ export function ActivityForm({ eventId, activityData }: ActivityFormProps) {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6">
-        <div className="flex gap-6">
-          <FormField
-            control={form.control}
-            name="startTime"
-            render={() => (
-              <FormItem className="flex w-full flex-col space-y-2">
-                <FormLabel className="font-semibold">Start time</FormLabel>
-                <FormControl>
-                  <Controller
-                    name="startTime"
-                    control={form.control}
-                    render={({ field }) => (
-                      <input
-                        type="time"
-                        id="start-time"
-                        {...field}
-                        className="h-10 rounded-md border border-border p-4 font-medium"
-                      />
-                    )}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="endTime"
-            render={() => (
-              <FormItem className="flex w-full flex-col space-y-2">
-                <FormLabel className="font-semibold">End time</FormLabel>
-                <FormControl>
-                  <Controller
-                    name="endTime"
-                    control={form.control}
-                    render={({ field }) => (
-                      <input
-                        type="time"
-                        id="end-time"
-                        {...field}
-                        className="h-10 rounded-md border border-border p-4 font-medium"
-                      />
-                    )}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
         <FormField
           control={form.control}
           name="title"
@@ -230,9 +91,9 @@ export function ActivityForm({ eventId, activityData }: ActivityFormProps) {
               <FormLabel className="font-semibold">Activity detail</FormLabel>
               <FormControl>
                 <textarea
+                  placeholder="Enter activity details"
                   className="w-full rounded-md border p-4 font-medium placeholder:text-textSub"
                   rows={4}
-                  placeholder="Enter activity details"
                   {...field}
                 />
               </FormControl>
@@ -244,7 +105,7 @@ export function ActivityForm({ eventId, activityData }: ActivityFormProps) {
           type="submit"
           className="h-10 w-full rounded-full text-base font-bold"
         >
-          {activityData ? "Update activity" : "Add activity"}
+          Submit
         </Button>
       </form>
     </Form>
--- frontend/src/lib/actions/event/timeline.ts
+++ frontend/src/lib/actions/event/timeline.ts
@@ -2,86 +2,28 @@
 
 import { getServerAxiosInstance } from "@/lib/api/axios-server";
 import { TimelineType } from "@/types/timeline";
-import { AxiosError } from "axios";
 
 // Add activity
 export const addActivity = async ({
-  requestData,
+  activityData,
   eventId,
 }: {
-  requestData: Omit<TimelineType, "id">;
+  activityData: Omit<TimelineType, "id">;
   eventId: string;
-}): Promise<{
-  success: boolean;
-  message: string;
-}> => {
+}) => {
   try {
     const axiosInstance = await getServerAxiosInstance();
     const response = await axiosInstance.post(
       `/events/${eventId}/timelines`,
-      requestData,
+      activityData,
     );
 
     return response.data;
   } catch (err) {
-    if (err instanceof AxiosError) {
-      throw new AxiosError(err.response?.data.message);
+    if (err instanceof Error) {
+      throw new Error(err.message || "Unknown error occurred");
     } else {
-      throw new Error("Failed to create activity. Please try again.");
-    }
-  }
-};
-
-// Update activity
-export const updateActivity = async ({
-  requestData,
-  eventId,
-  activityId,
-}: {
-  requestData: Omit<TimelineType, "id">;
-  eventId: string;
-  activityId: number;
-}): Promise<{
-  success: boolean;
-  message: string;
-}> => {
-  try {
-    const axiosInstance = await getServerAxiosInstance();
-    const response = await axiosInstance.put(
-      `/events/${eventId}/timelines/${activityId}`,
-      requestData,
-    );
-
-    return response.data;
-  } catch (err) {
-    if (err instanceof AxiosError) {
-      throw new AxiosError(err.response?.data.message);
-    } else {
-      throw new AxiosError("Failed to update activity. Please try again.");
-    }
-  }
-};
-
-// Delete activity
-export const deleteActivity = async (
-  eventId: string,
-  activityId: number,
-): Promise<{
-  success: boolean;
-  message: string;
-}> => {
-  try {
-    const axiosInstance = await getServerAxiosInstance();
-    const response = await axiosInstance.delete(
-      `events/${eventId}/timelines/${activityId}`,
-    );
-
-    return response.data;
-  } catch (err) {
-    if (err instanceof AxiosError) {
-      throw new AxiosError(err.response?.data.message);
-    } else {
-      throw new Error("Failed to delete activity. Please try again.");
+      throw new Error("An unexpected error occurred");
     }
   }
 };
