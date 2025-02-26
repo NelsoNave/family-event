82cd15ff35d02e1c086ba32c15d6468161f91e20
BenOno <bentoki.1213@gmail.com>
2025-02-26 15:47:02 -0800

    done: add & delete emp participants

--- backend/src/routes/event.routes.ts
+++ backend/src/routes/event.routes.ts
@@ -59,6 +59,18 @@ eventRouter.patch(
   "/:event_id/participants/:participant_id/attendance",
   eventParticipantsController.updateParticipantAttendance,
 );
+eventRouter.delete(
+  "/:event_id/participants/:participant_id",
+  eventParticipantsController.deleteParticipant,
+);
+eventRouter.post(
+  "/:event_id/participants/temporary",
+  eventParticipantsController.addTemporaryParticipant,
+);
+eventRouter.delete(
+  "/:event_id/participants/temporary/:participant_id",
+  eventParticipantsController.deleteTemporaryParticipant,
+);
 
 //Album
 eventRouter.get(
