2603810e83a852bf63a4950d868801b0122e8abd
Risa Yamamoto <yamari2422@gmail.com>
2025-02-27 14:53:33 -0800

    chore: undo unnecessary changes(modal)

--- frontend/src/components/features/modal.tsx
+++ frontend/src/components/features/modal.tsx
@@ -20,10 +20,8 @@ type ModalProps = {
   title: string;
   description?: string;
   button?: ReactNode;
-  deleteAction: (
-    id?: string | string[],
-  ) => Promise<{ success: boolean; message: string }>;
-  id?: string | string[];
+  deleteAction: (id?: string) => Promise<{ success: boolean; message: string }>;
+  id?: string;
   deleteErrorMessage: string;
   onSuccess?: () => void;
 };
@@ -41,7 +39,7 @@ export default function Modal({
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  const { toast } = useToast()
+  const {toast} = useToast()
   const { signOut } = useClerk()
 
   const handleDelete = async (e: FormEvent) => {
